4ceb7a48df1a0b61bf7ea827f6d886023abcf892
Alexander Shvets <alexander.shvets@gmail.com>
2010-08-05 12:36:11 -0400

    modifying chain of responsibility pattern

--- behavioral/chain_of_responsibility.rb
+++ behavioral/chain_of_responsibility.rb
@@ -1,50 +1,41 @@
-# chain-of-responsibility.bsh
+# chain-of-responsibility.rb
 
 # Avoid coulpling the sender of a request to it's receiver by giving more than
-# one object a chance to handle the request. Chain the receiving object and pass
+# one object a chance to handle the request. Chains the receiving object and passes
 # the request along the chain until an object handles it.
 
 # 1. type interface
 
-class Handler
+class Chain
   def handle
   end
 
-  def next_handler=(handler)
+  def initialize(next_in_chain)
+    @next_in_chain = next_in_chain
   end
 end
 
 # 2. type implementation
 
-class MyHandler < Handler
-  def initialize(name)
+class MyChain < Chain
+  def initialize(name, next_in_chain=nil)
+    super(next_in_chain)
+    
     @name = name
   end
   
   def handle
     puts "Handling by " + @name + "."
 
-    if(@next_handler != nil)
-      @next_handler.handle
+    if(@next_in_chain != nil)
+      @next_in_chain.handle
     end
-  end
-
-  def next_handler=(next_handler)
-    @next_handler = next_handler
   end
 end
 
 # 3. test
 
-handler1 = MyHandler.new("handler1")
-handler2 = MyHandler.new("handler2")
-handler3 = MyHandler.new("handler3")
-handler4 = MyHandler.new("handler4")
-handler5 = MyHandler.new("handler5")
+chain = MyChain.new("chain1", MyChain.new("chain2", MyChain.new("chain3", MyChain.new("chain5", MyChain.new("chain4")))))
 
-handler1.next_handler = handler2
-handler2.next_handler = handler5
-handler5.next_handler = handler3
-handler3.next_handler = handler4
+chain.handle
 
-handler1.handle
